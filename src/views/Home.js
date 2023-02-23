1b1144e41cdd8d8ccb6552b951d9f70c6732c43e
MuDzYNN <MudzynDev@gmail.com>
2023-02-23 22:50:11 +0100

    test

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
         <div className="home">
             <Header />
             <div className="hero" data-aos="fade-up">
-                <h1>A co to za gra?</h1>
+                <h1>TEST</h1>
                 <h2>Nic szczególnego, ale możesz sprawdzić sam ;D</h2>
                 <Link to="/auth"><div className="btn">Przejdź do panelu</div></Link>
             </div>
